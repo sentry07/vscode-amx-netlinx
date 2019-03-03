8f1539c69b9b4ab77844e9eee0e6fe033548d8d4
sentry07 <sentry07@gmail.com>
2019-03-03 01:26:05 -0800

    Split languages for AXS and AXI,AXB for compiling purposes

--- out/extension.js
+++ out/extension.js
@@ -5,10 +5,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const vscode = require("vscode");
 // import * as fs from "fs";
 // import * as path from "path";
-const svg = require("./visualizer");
-const api = require("./api");
 // import { stat } from 'fs';
-// import { getSimplSharpLibraries } from './api-util';
 // import { stat } from 'fs';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -19,137 +16,44 @@ function activate(context) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let previewUri = vscode.Uri.parse('simpl-visualize://authority/simpl-visualize');
-    class TextDocumentContentProvider {
-        constructor() {
-            this._onDidChange = new vscode.EventEmitter();
-        }
-        update(uri) {
-            this._onDidChange.fire(uri);
-        }
-        provideTextDocumentContent(uri) {
-            return this.createVisualizer();
-        }
-        createVisualizer() {
-            let editor = vscode.window.activeTextEditor;
-            if (editor) {
-                if (!(editor.document.languageId === 'simpl+')) {
-                    return this.errorSnippet("A SIMPL+ file is not open.");
-                }
-                else {
-                    return this.extractVisualizer();
-                }
-            }
-            else {
-                return this.errorSnippet("A SIMPL+ file is not open.");
-            }
-        }
-        extractVisualizer() {
-            let parser = new svg.VisualizerParse();
-            parser.parseSimplPlus();
-            let viewer = new svg.SVGCreator(parser);
-            return viewer.returnSVG();
-        }
-        errorSnippet(error) {
-            return `
-				<body>
-					${error}
-				</body>`;
-        }
-        get onDidChange() {
-            return this._onDidChange.event;
-        }
-    }
-    let provider = new TextDocumentContentProvider();
-    let registration = vscode.workspace.registerTextDocumentContentProvider('simpl-visualize', provider);
-    vscode.workspace.onDidChangeTextDocument((e) => {
-        if (vscode.window.activeTextEditor && e.document === vscode.window.activeTextEditor.document) {
-            provider.update(previewUri);
-        }
+    let netlinx_compile = vscode.commands.registerCommand('extension.netlinx_compile', () => {
+        processNetlinx();
     });
-    vscode.window.onDidChangeTextEditorSelection((e) => {
-        if (e.textEditor === vscode.window.activeTextEditor) {
-            provider.update(previewUri);
-        }
-    });
-    let series3_compile = vscode.commands.registerCommand('extension.simplCC_Series3', () => {
-        processSimpl("\\target series3");
-    });
-    let series2and3_compile = vscode.commands.registerCommand('extension.simplCC_Series2and3', () => {
-        processSimpl("\\target series2 series3");
-    });
-    let series3_compileAll = vscode.commands.registerCommand("extension.simplCC_Series3All", () => {
-        let foundFiles = vscode.workspace.findFiles('*.usp');
-        let term = vscode.window.createTerminal('simplCC', vscode.workspace.getConfiguration("simpl").terminalLocation);
-        let compiler = new SimplCompiler();
-        term.show();
-        foundFiles.then(files => {
-            if (files.length) {
-                files.forEach(e => {
-                    compiler.filepaths.push(e.fsPath);
-                });
-                term.sendText(compiler.buildCommand("\\target series3"));
-            }
-            else {
-                vscode.window.showErrorMessage("No .usp files found");
-            }
-        });
-    });
-    let simpl_visualize = vscode.commands.registerCommand("extension.simpl_visualize", () => {
-        return vscode.commands.executeCommand('vscode.previewHtml', previewUri, vscode.ViewColumn.Two, 'SIMPL Preview').then((success) => {
-        }, (reason) => {
-            vscode.window.showErrorMessage(reason);
-        });
-        // let uri = vscode.Uri.parse('file:///resources/test.html');
-        // return vscode.commands.executeCommand('vscode.previewHtml', uri );
-    });
-    let help_command = vscode.commands.registerCommand("extension.simpl_help", () => {
-        let helpLocation = vscode.workspace.getConfiguration("simpl").helpLocation;
-        let term = vscode.window.createTerminal('simpl', vscode.workspace.getConfiguration("simpl").terminalLocation);
+    let help_command = vscode.commands.registerCommand("extension.netlinx_help", () => {
+        let helpLocation = vscode.workspace.getConfiguration("netlinx").helpLocation;
+        let term = vscode.window.createTerminal('netlinx', vscode.workspace.getConfiguration("netlinx").terminalLocation);
         term.sendText("\"" + helpLocation + "\"");
     });
-    let open_api = vscode.commands.registerCommand("extension.simplCC_API", () => {
-        let editor = vscode.window.activeTextEditor;
-        if (editor) {
-            let newAPI = new api.API(editor);
-            newAPI.openAPIFiles();
-        }
-    });
-    context.subscriptions.push(series3_compile);
-    context.subscriptions.push(series2and3_compile);
-    context.subscriptions.push(series3_compileAll);
+    context.subscriptions.push(netlinx_compile);
     context.subscriptions.push(help_command);
-    context.subscriptions.push(simpl_visualize);
-    context.subscriptions.push(open_api);
-    context.subscriptions.push(registration);
 }
 exports.activate = activate;
-function processSimpl(args) {
+function processNetlinx(args) {
     let editor = vscode.window.activeTextEditor;
     if (!editor) {
-        vscode.window.showErrorMessage("Please open a valid USP file.");
+        vscode.window.showErrorMessage("Please open a valid AXS file.");
         return;
     }
     let doc = editor.document;
-    if (doc.languageId === "simpl+") {
+    if (doc.languageId === "netlinx-source") {
         let savedDoc = doc.save();
         savedDoc.then(() => {
-            let compiler = new SimplCompiler();
+            let compiler = new NetlinxCompiler();
             compiler.filepaths.push(doc.fileName);
-            let term = vscode.window.createTerminal('simplCC', vscode.workspace.getConfiguration("simpl").terminalLocation);
+            let term = vscode.window.createTerminal('netlinx', vscode.workspace.getConfiguration("netlinx").terminalLocation);
             term.show();
             term.sendText(compiler.buildCommand(args));
         });
     }
     else {
-        vscode.window.showErrorMessage("Please open a valid USP file.");
+        vscode.window.showErrorMessage("Please open a valid AXS file.");
         return;
     }
 }
-class SimplCompiler {
+class NetlinxCompiler {
     constructor() {
         this.filepaths = [];
-        this.compilerPath = "\"" + vscode.workspace.getConfiguration("simpl").compiler + "\"";
+        this.compilerPath = "\"" + vscode.workspace.getConfiguration("netlinx").compiler + "\"";
         console.log(this.compilerPath);
     }
     buildCommand(args) {
@@ -158,9 +62,8 @@ class SimplCompiler {
             filepathConcat += "\"" + element + "\" ";
         });
         return this.compilerPath +
-            " \\rebuild " +
-            filepathConcat + " " +
-            args;
+            " " +
+            filepathConcat;
     }
 }
 // this method is called when your extension is deactivated
