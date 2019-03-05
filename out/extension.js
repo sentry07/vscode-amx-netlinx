2d6b65bff25f4f10e3447a2edfe51a168b1b777f
sentry07 <sentry07@gmail.com>
2019-03-04 22:30:05 -0800

    Added code beautification function/context command

--- out/extension.js
+++ out/extension.js
@@ -1,23 +1,14 @@
 'use strict';
 Object.defineProperty(exports, "__esModule", { value: true });
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+
 const vscode = require("vscode");
-// import * as fs from "fs";
-// import * as path from "path";
-// import { stat } from 'fs';
-// import { stat } from 'fs';
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+
 function activate(context) {
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "amx-netlinx" is now active!');
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
+    let netlinx_format = vscode.commands.registerCommand('extension.netlinx_format', () => {
+        fixIndentation();
+    });
     let netlinx_compile = vscode.commands.registerCommand('extension.netlinx_compile', () => {
-        processNetlinx();
+        compileNetlinx();
     });
     let transfer_command = vscode.commands.registerCommand("extension.netlinx_transfer", () => {
         let transferLocation = vscode.workspace.getConfiguration("netlinx").transferLocation;
@@ -34,11 +25,15 @@ function activate(context) {
         let term = vscode.window.createTerminal('netlinx', vscode.workspace.getConfiguration("netlinx").terminalLocation);
         term.sendText("\"" + helpLocation + "\"");
     });
+    context.subscriptions.push(netlinx_format);
     context.subscriptions.push(netlinx_compile);
+    context.subscriptions.push(transfer_command);
+    context.subscriptions.push(diag_command);
     context.subscriptions.push(help_command);
 }
 exports.activate = activate;
-function processNetlinx(args) {
+
+function compileNetlinx(args) {
     let editor = vscode.window.activeTextEditor;
     if (!editor) {
         vscode.window.showErrorMessage("Please open a valid AXS file.");
@@ -57,6 +52,50 @@ function processNetlinx(args) {
     }
     else {
         vscode.window.showErrorMessage("Please open a valid AXS file.");
+        return;
+    }
+}
+
+function fixIndentation(args) {
+    let editor = vscode.window.activeTextEditor;
+    if (!editor) {
+        vscode.window.showErrorMessage("Please open a valid Netlinx file.");
+        return;
+    }
+    let doc = editor.document;
+    var firstLine = doc.lineAt(0);
+    var lastLine = doc.lineAt(doc.lineCount - 1);
+    var textRange = new vscode.Range(0, 
+                                     firstLine.range.start.character, 
+                                     doc.lineCount - 1, 
+                                     lastLine.range.end.character);
+    if (doc.languageId === "netlinx-source") {
+        let indentLevel = 0;
+        let outputText = "";
+        let reIncrease = new RegExp("^((?!\\/\\/).)*(\\{[^{}\\v]*|\\([^()\\v]*|\\[[^\\[\\]\\v]*)$");
+        let reDecrease = new RegExp("^((?!.*?\\/\\*).*\\*\\/)?\\s*[\\}\\]\\)].*$");
+        let docText = editor.document.getText();
+        console.debug(docText);
+        let docLines = docText.split(/\r?\n/);
+        for (var line = 0; line < docLines.length; line++) {
+            if (reIncrease.test(docLines[line])) {
+                outputText = outputText + ('\t'.repeat(indentLevel)) + docLines[line].trimLeft() + "\r";
+                indentLevel = indentLevel + 1;
+            }
+            else if (reDecrease.test(docLines[line])) {
+                indentLevel = indentLevel - 1;
+                outputText = outputText + ('\t'.repeat(indentLevel)) + docLines[line].trimLeft() + "\r";
+            }
+            else {
+                outputText = outputText + ('\t'.repeat(indentLevel)) + docLines[line].trimLeft() + "\r";
+            }
+        };
+        editor.edit(editBuilder => {
+            editBuilder.replace(textRange,outputText);
+        });
+    }
+    else {
+        vscode.window.showErrorMessage("Please open a valid Netlinx file.");
         return;
     }
 }
